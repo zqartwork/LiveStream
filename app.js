c5e18e1a42d880c92d14f054cc6c65153b478cd7
zqartwork <zqartworkofficial@gmail.com>
2019-07-24 18:04:07 +0800

    節目表架構結束

--- app.js
+++ app.js
@@ -8,3 +8,41 @@ let stickyNav = function () {
         }
     })
 }
+let selectCustom = function () {
+    let cs = $('.custom_select');
+    let csMenu = $('.select_menu');
+    let csMenuOpt = $('.select_menu li')
+
+    cs.click(function () {
+        $(this).attr('tabindex', 1).focus();
+        $(this).toggleClass('active');
+        $(this).children('.select_menu').slideToggle(300);
+    })
+    cs.focusout(function () {
+        $(this).removeClass('active');
+        $(this).children('.select_menu').slideUp(300);
+    })
+    csMenuOpt.click(function () {
+        $(this).parents('.custom_select').find('.selected').text($(this).text());
+        $(this).parents('.custom_select').find('#data_input').attr('value', $(this).attr('id'));
+    })
+}
+let activeTag = function () {
+    let target = $('.filter_tag');
+    let targetProgram = $('.program-type');
+
+    target.click(function () {
+        $(this).attr('tabindex', 1).focus();
+        $(this).toggleClass('active');
+    })
+    target.focusout(function () {
+        $(this).removeClass('active');
+    })
+    targetProgram.click(function () {
+        $(this).attr('tabindex', 1).focus();
+        $(this).toggleClass('active');
+    })
+    targetProgram.focusout(function () {
+        $(this).removeClass('active');
+    })
+}
